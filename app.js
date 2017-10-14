c30d8e47aeb65e305e624ee2f52ce34c6eeb733c
reng <1837895991@qq.com>
2017-10-14 09:46:59 +0800

    change the method to start app

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-// 应用程序的入口文件 2017-10-13
+/**
+ * 应用程序入口
+ * 
+ * author:reng
+ * 
+ * email:1837895991@qq.com
+ * 
+ * build-time:2017-10-13
+ */
+
+// 加载express模块
+var express = require('express');
+// 创建app应用 === 等价于原生的node中的http.createServer();
+var app =express();
+
+// 首页
+app.get('/',function(req,res,next){
+    res.send('<h1>hello world!!</h1>');
+});
+
+// 监听http请求
+app.listen(9000);
