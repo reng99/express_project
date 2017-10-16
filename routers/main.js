a0fd875d49d32e8d59e6e8f6025f7b59257ce020
reng <1837895991@qq.com>
2017-10-16 13:12:30 +0800

    add relative link

--- routers/main.js
+++ routers/main.js
@@ -2,14 +2,22 @@
 var express = require('express');
 var router = express.Router();
 
+// 首页
 router.get('/',function(req,res,next){
     // 参数二为分配的模版信息
     res.render('main/index.html',{});
 });
 
+// 登录页
 router.get('/login',function(req,res,next){
     // 参数二为分配的模版信息
     res.render('main/login.html',{});
+});
+
+// 注册页
+router.get('/register/index',function(req,res,next){
+    // 参数二为分配的模版信息
+    res.render('main/register/index.html',{});
 });
 
 module.exports = router;
