9dab8b183372e8053c705c3adaf32a117a54a16f
reng <1837895991@qq.com>
2017-10-25 11:30:29 +0800

    add admin redirect

--- routers/main.js
+++ routers/main.js
@@ -2,8 +2,12 @@
 var express = require('express');
 var router = express.Router();
 
-// 首页
+// 前台首页
 router.get('/index.html',function(req,res,next){
+    if(req.userInfo.isAdmin){// 判断是否是管理员,重定向
+        res.redirect('/admin/index.html');
+        return;
+    }
     // 参数二为分配的模版信息
     res.render('main/index.html',{
         userInfo: req.userInfo // 为模版设置一个userInfo 并从req中获取相关值
